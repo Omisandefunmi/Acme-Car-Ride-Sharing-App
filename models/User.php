57041fa960d28ff572ab9acfffefee59d1a29198
omisandefunmi <omisandefunmi@gmail.com>
2023-04-17 00:05:44 +0100

    "added register view"

---
+++ models/User.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $uid
+ * @property string $username
+ * @property string $email
+ * @property string $password
+ * @property int $status
+ * @property int $contact_email
+ * @property int $contact_phone
+ * @property string $created
+ * @property string $updated
+ *
+ * @property Message[] $messages
+ * @property Message[] $messages0
+ * @property PhoneNumber[] $phoneNumbers
+ * @property Trip[] $trips
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['uid', 'username', 'email', 'password', 'updated'], 'required'],
+            [['status', 'contact_email', 'contact_phone'], 'integer'],
+            [['created', 'updated'], 'safe'],
+            [['uid', 'password'], 'string', 'max' => 60],
+            [['username'], 'string', 'max' => 45],
+            [['email'], 'string', 'max' => 255],
+            [['uid'], 'unique'],
+            [['email'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'uid' => Yii::t('app', 'Uid'),
+            'username' => Yii::t('app', 'Username'),
+            'email' => Yii::t('app', 'Email'),
+            'password' => Yii::t('app', 'Password'),
+            'status' => Yii::t('app', 'Status'),
+            'contact_email' => Yii::t('app', 'Contact Email'),
+            'contact_phone' => Yii::t('app', 'Contact Phone'),
+            'created' => Yii::t('app', 'Created'),
+            'updated' => Yii::t('app', 'Updated'),
+        ];
+    }
+
+    /**
+     * Gets query for [[Messages]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMessages()
+    {
+        return $this->hasMany(Message::class, ['from_user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Messages0]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMessages0()
+    {
+        return $this->hasMany(Message::class, ['to_user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[PhoneNumbers]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPhoneNumbers()
+    {
+        return $this->hasMany(PhoneNumber::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Trips]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTrips()
+    {
+        return $this->hasMany(Trip::class, ['user_id' => 'id']);
+    }
+}
---
+++ views/site/register.php
@@ -0,0 +1,41 @@
+<?php
+/** @var yii\web\View $this */
+/** @var yii\bootstrap5\ActiveForm $registerForm */
+
+use yii\bootstrap5\ActiveForm;
+use yii\bootstrap5\Html;
+use app\models\User;
+
+$this->title = 'Register';
+?>
+
+<h1><?= Html::encode($this->title) ?></h1>
+
+<p>Please fill out the following fields to sign up:</p>
+
+<?php $registerForm = ActiveForm::begin([
+    'id' => 'register-form',
+    'layout' => 'horizontal',
+    'fieldConfig' => [
+        'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+        'labelOptions' => ['class' => 'col-lg-1 control-label'],
+    ],
+]); ?>
+
+<?= $registerForm->errorSummary($newUser) ?>
+<?= $registerForm->field($newUser, 'username')->textInput(['autofocus' => true]) ?>
+<?= $registerForm->field($newUser, 'email')->textInput() ?>
+<?= $registerForm->field($newUser, 'password')->passwordInput(['value' => '']) ?>
+
+<?php //= $registerForm->field($newUser, 'rememberMe')->checkbox([
+//    'template' => "<div class=\"offset-lg-1 col-lg-3 custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+//]) ?>
+
+<div class="form-group">
+    <div class="offset-lg-1 col-lg-11">
+        <?= Html::submitButton('register', ['class' => 'btn btn-primary', 'name' => 'register-button']); ?>
+
+    </div>
+</div>
+
+<?php ActiveForm::end(); ?>
